2945017d42c257ee89e830c36349a0d85c8aa898
nik frank <nikbfrank@gmail.com>
2014-07-13 01:14:09 +0300

    subm

---
+++ controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('calculator')
+    .controller('Ctrl', function($scope){
+
+	$scope.num1 = 0;
+	$scope.num2 = 0;
+
+	$scope.ans = 0;
+
+	$scope.add = function(){
+	    console.log(typeof $scope.num1);
+	    $scope.ans = $scope.num1 + $scope.num2;
+	};
+
+	$scope.sub = function(){
+	    $scope.ans = $scope.num1 - $scope.num2;
+	};
+
+	$scope.div = function(){
+	    $scope.ans = $scope.num1 / $scope.num2;
+	};
+
+	$scope.mul = function(){
+	    $scope.ans = $scope.num1 * $scope.num2;
+	};
+
+    });

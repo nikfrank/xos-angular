a5609cdecaaaef4ce7f15dfec24aef7c98371855
nik frank <nikbfrank@gmail.com>
2014-12-25 09:21:59 +0200

    color schedule

--- scripts/directives/day.js
+++ scripts/directives/day.js
@@ -31,6 +31,36 @@ angular.module('gft')
 		console.log('make on ', day);
 	    };
 
+
+// this watch should self destruct
+	    $scope.$watch('workouts', function(n,o){
+		if(!n) return;
+		if(n.length){
+		    
+		    var types = n.reduce(function(p,c){
+			p.flex += c.flex;
+			p.str += c.str;
+			p.cardio += c.cardio;
+			
+			return p;
+
+		    }, {flex:0, str:0, cardio:0});
+
+		    var maxtval = Math.max(types.str, types.flex, types.cardio);
+
+		    if(maxtval === types.flex){
+			$scope.dayclass = 'busy-day-flex';
+		    }else if(maxtval === types.str){
+			$scope.dayclass = 'busy-day-str';
+		    }else if(maxtval === types.cardio){
+			$scope.dayclass = 'busy-day-cardio';
+		    }
+
+		}else{
+		    $scope.dayclass = '';
+		}
+	    });
+
 	}
     };
   });
